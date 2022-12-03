7d8093d6400c7b528be1a267793d143c05694c78
kangxie <arking1981@gmail.com>
2022-12-03 08:13:09 -0700

    day 3

---
+++ day3.py
@@ -0,0 +1,48 @@
+def find_same_char_in_both_half(s):
+    h1 = s[:len(s) // 2]
+    h2 = s[len(s) // 2:]
+
+    return set(h1).intersection(set(h2))
+
+
+def get_score(c):
+    if 'a' <= c <= 'z':
+        return ord(c) - ord('a') + 1
+    if 'A' <= c <= 'Z':
+        return ord(c) - ord('A') + 27
+    return 0
+
+
+def get_pri_sum(filename):
+    res = 0
+    with open(filename) as f:
+        for line in f:
+            dupChar = list(find_same_char_in_both_half(line.strip()))[0]
+            res += get_score(dupChar)
+
+    return res
+
+
+def get_pri_sum_per_group(filename):
+    with open(filename) as f:
+        group = []
+        res = 0
+        for line in f:
+            if len(group) < 3:
+                group.append(line.strip())
+
+            if len(group) == 3:
+                badge = list(set(group[0]).intersection(set(group[1])).intersection(set(group[2])))[0]
+                res += get_score(badge)
+                group = []
+        return res
+
+
+if __name__ == '__main__':
+    filename = 'day3_sample.txt'
+    print(get_pri_sum(filename))
+    print(get_pri_sum_per_group(filename))
+
+    filename = 'day3_input.txt'
+    print(get_pri_sum(filename))
+    print(get_pri_sum_per_group(filename))
